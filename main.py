df8426b32b8887a0f42047f6d6ba238697de6c4f
Michi The Cat <mrzombsoriginal@gmail.com>
2026-03-01 22:28:03 +0300

    logs
    Система логов | Logs system

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 try: # Импорт библиотек
     print('Импорт библиотек...', end='', flush=True)
     import ollama # pip install ollama
-    import asyncio
     import os
     from random import randint
     from telegram import Bot # pip install python-telegram-bot
     from telegram.ext import Application, MessageHandler, filters
+    from datetime import datetime
 except Exception as e: input('\rКритическая ошибка:', e); quit()
 else: print('\rИмпорт библиотек завершён.')
 
@@ -16,7 +16,9 @@ KEY = False # Описание ниже | The description is below
 # Напишите KEY = {ключ}, если хотите, чтобы программа не искала ключ в файлах
 # Write KEY = False if you want the program to find the key in C:\MichiPythonFiles\GreenTeaBot\key
 # Write KEY = {key} if you want the program not to search for the key in the files
-ID = -1002622534151 # Поменяйте на свой | Swap it for your own
+ID = -1002622534151 # Поменяйте на свой ID группы | Swap it for your own group ID
+RAN = 0.1 # Шанс, что бот ответит на сообщение | Chance that the bot will reply to the message
+LOGS = True # Если нужны или не нужны логи | If logs are needed or not needed
 
 
 # Достать ключ
@@ -34,30 +36,40 @@ if not KEY:
     else: print('\rУспешно достали ключ.')
 
 
-# Основной цикл
+# Создаю логи
+log_data = ''
+def logs(text):
+    global log_data
+    if LOGS:
+        if not log_data:
+            now = datetime.now()
+            data = now.strftime('log_%d-%m-%Y_%H-%M.txt')
+            log_data = data
+        now = datetime.now()
+        data = now.strftime("%d.%m.%Y %H:%M")
+        with open(f'logs/{log_data}', 'a', encoding='utf-8') as f:
+            f.write(f'<{data}>\n{text}\n\n')
+
+
+# Основная функция
 async def handle_message(update, context):
-    if update.message.chat_id != ID:
-        return
-
-    user_text = update.message.text
-    if not user_text:
-        return
-
-    reply_text = f"Вы написали: {user_text}"
-
-    await update.message.reply_text(reply_text)
-
-def main():
-    application = Application.builder().token(KEY).build()
-
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
-    print("Бот запущен и слушает сообщения в группе...")
-    application.run_polling()
+    print('\n> Вижу сообщение')
+    if (_ := randint(0, int(RAN*100))) == 0:
+        try:
+            if update.message.chat_id != ID:
+                return
+            user_text = update.message.text
+            print(f'  Содержание: {user_text}'); logs(f'> Содержание: {user_text}')
+            if not user_text:
+                return
+            reply_text = f"Вы написали: {user_text}"
+            await update.message.reply_text(reply_text)
+        except Exception as e: print('< Ошибка:', e); logs((f'< Ошибка: {e}'))
+    else: print(f'< Сообщение осталось без ответа: {_} != 0'); logs('< Сообщение осталось без ответа')
 
 
-main()
-
-
-# >> https://t.me/c/2622534151/20065
-# > Сначала научился писать сообщение в чат, потом научился доставать сообщения и возвращать их с ответом.. Теперь осталось подключить Ollama  и дописать рандом
+# Основной цикл/инициализация
+application = Application.builder().token(KEY).build()
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+print("Бот запущен..."); logs('Бот запущен')
+application.run_polling()
