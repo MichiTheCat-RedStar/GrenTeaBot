a3e2d2d49654628658a77ff9f49e04afbd73c8de
Michi The Cat <mrzombsoriginal@gmail.com>
2026-03-01 21:25:26 +0300

    Hello World

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-KEY = False     # Описание ниже | The description is below
-# Напишите KEY = False, если хотите, чтобы программа нашла ключ в C:\MyPythonFiles\GreenTeaBot\key
-# Напишите KEY = {ключ}, если хотите, чтобы программа не искала ключ в файлах
-# Write KEY = False if you want the program to find the key in C:\MyPythonFiles\GreenTeaBot\key
-# Write KEY = {key} if you want the program not to search for the key in the files
+try: # Импорт библиотек
+    print('Импорт библиотек...', end='', flush=True)
+    import ollama # pip install ollama
+    import asyncio
+    import os
+    from random import randint
+    from telegram import Bot # pip install python-telegram-bot
+except Exception as e: input('\rКритическая ошибка:', e); quit()
+else: print('\rИмпорт библиотек завершён.')
 
-# TODO прямо сейчас:
-# 1. импорт библиотек
-# 2. try/exec везде
-# 3. Система логов: нужна ли она тут, если инструменты не используются?
-# 4. создание остальной части кода и написание нового TODO
+
+# Вставить ключ или директорию (только для Windows) | Insert key or directory (Windows only)
+KEY = False # Описание ниже | The description is below
+# Напишите KEY = False, если хотите, чтобы программа нашла ключ в C:\MichiPythonFiles\GreenTeaBot\key
+# Напишите KEY = {ключ}, если хотите, чтобы программа не искала ключ в файлах
+# Write KEY = False if you want the program to find the key in C:\MichiPythonFiles\GreenTeaBot\key
+# Write KEY = {key} if you want the program not to search for the key in the files
+CHANNEL_ID = '-1002622534151' # Поменяйте на свой | Swap it for your own
+
+
+# Достать ключ
+if not KEY:
+    try:
+        print('Достаём ключ...', end='', flush=True)
+        with open('C:/MichiPythonFiles/GreenTeaBot/key', 'r', encoding='utf-8') as f: KEY = f.read()
+    except Exception as e:
+        os.makedirs('C:/MichiPythonFiles', exist_ok=True)
+        os.makedirs('C:/MichiPythonFiles/GreenTeaBot', exist_ok=True)
+        with open('C:/MichiPythonFiles/GreenTeaBot/key', 'w', encoding='utf-8') as f:
+            f.write('Напишите свой ключ здесь | Write your key here')
+        input(f'\rКритическая ошибка: {e}\nНо папка была создана в деректории C:/MichiPythonFiles/GreenTeaBot/key')
+        quit
+    else: print('\rУспешно достали ключ.')
+
+
+# Основной цикл
+async def send_post_with_comment():
+    bot = Bot(token=KEY)
+    await bot.send_message(
+            chat_id=CHANNEL_ID,
+            text="Hello World!"
+        )
+
+asyncio.run(send_post_with_comment())
